b63cb93393edbd4db8b16d950aaa9c2b30348f25
Alberto Capone <albertocapone@outlook.it>
2020-04-21 13:02:04 +0200

    JS - refactoring, documentazione e implementazione pulizia dei risultati di ricerca ad ogni click -

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
 $(document).ready(
   function() {
-
-    var template = Handlebars.compile($('#template_media').html());
+    // riferimenti html
+    var searchResultsBox = $('.search_results');
+    var template = Handlebars.compile($('#template_media').html());  //predispongo template
 
     $('#go').click(
       function (){
-        var userSearch = $('input').val();
-
+        searchResultsBox.html("");  //pulisco la box coi risultati di ricerca
+        var userSearch = $('input').val(); //registro la stringa di ricerca inserita dall'utente
         $.ajax({
           url: "https://api.themoviedb.org/3/search/movie",
           method: "GET",
           data: {
-            api_key: "99778220f31eec4fabbbe1461237e9d0",
-            query: userSearch,
+            api_key: "99778220f31eec4fabbbe1461237e9d0",  //mia api key
+            query: userSearch,                           //associo stringa ricerca utente
           },
           success: function (data){
-            var results = data.results;
-            for (var key of results){
-            var context = {
+            var results = data.results;                //registro array di oggetti estratto dalla proprieta results dell'oggetto data
+            for (var key of results){                 //ciclo su array results   (key == results[k] in un for classico)
+            var context = {                           //ad ogni iterazione sovrascrivo nell'oggetto context le proprieta di key di cui ho bisogno
               title: key.title,
               originalTitle: key.original_title,
               language: key.original_language,
               score: key.vote_average
             }
-            $('.search_results').append(template(context));
+            searchResultsBox.append(template(context));   //inietto nel box dei risultati di ricerca il mio template valorizzato attraverso l'oggetto context dell'iterazione corrente 
           }
           },
           error: function (request, state, errors){
