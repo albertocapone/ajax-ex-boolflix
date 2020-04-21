796bfb7fc83426ac5c81dcbdac7c94a7000da5fe
Alberto Capone <albertocapone@outlook.it>
2020-04-21 12:13:06 +0200

    JS (milestone 1 completo) - implementata ricerca film -

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 $(document).ready(
   function() {
-    console.log("ready");
 
+    var template = Handlebars.compile($('#template_media').html());
 
+    $('#go').click(
+      function (){
+        var userSearch = $('input').val();
 
-
-  }
-);
+        $.ajax({
+          url: "https://api.themoviedb.org/3/search/movie",
+          method: "GET",
+          data: {
+            api_key: "99778220f31eec4fabbbe1461237e9d0",
+            query: userSearch,
+          },
+          success: function (data){
+            var results = data.results;
+            for (var key of results){
+            var context = {
+              title: key.title,
+              originalTitle: key.original_title,
+              language: key.original_language,
+              score: key.vote_average
+            }
+            $('.search_results').append(template(context));
+          }
+          },
+          error: function (request, state, errors){
+            alert(errors);
+          }
+        });
+    });
+});
