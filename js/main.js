a7f2d90de2470c38b9dfb9a18a4053844df3b4e3
Alberto Capone <albertocapone@outlook.it>
2020-04-22 13:47:06 +0200

    JS - implementata associazione bandiere a lingue media -

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@ $(document).ready(
     var searchResultsBox = $('.search_results');
     var template = Handlebars.compile($('#template_media').html());  //predispongo template
 
+    function flagIt(language){
+      var toFlag = {          //creo un oggetto di corrispondenze codici paesi -> img paesi; ogni stringa-img verrà inserita in img all'interno del template
+        it: "img/it.png",
+        gb: "img/gb.png",
+        us: "img/us.png",
+        fr: "img/fr.png",
+        es: "img/es.png",
+        pt: "img/pt.png"
+      };
+      return toFlag[language] || "";      //ritorno la stringa-img risultante dall' accesso all'oggetto attraverso il parametro language (che è la prop language del media corrente) oppure se non esiste una stinga vuota
+    }
+
+    function rateIt(vote){
+      return; //stars
+    }
+
     function callTheAPI(query, searchingFor){
       $.ajax({
         url: "https://api.themoviedb.org/3/search/" + searchingFor, //l'url viene modificato in base alla ricerca
@@ -14,19 +30,23 @@ $(document).ready(
         },
         success: function (data){
           var results = data.results;                //registro array di oggetti estratto dalla proprieta results dell'oggetto data
+          console.log(results);
           for (var media of results){                 //ciclo su array results   (media == results[k] in un for classico)
+            console.log(media);
             if(searchingFor == "movie"){
               var context = {                        //ad ogni iterazione sovrascrivo nell'oggetto context le proprieta di key di cui ho bisogno
                 title: media.title,
                 originalTitle: media.original_title,
-                language: media.original_language,
+                flag: flagIt(media.original_language),  //chiamo la funzione flagIt (riga 7) passandogli come parametro la prop che contiene la lingua del media corrente
+                language: (flagIt(media.original_language) === "") ? media.original_language : "", //se non è associata alcuna img per il codice lingua allora ritorno il semplice codice lingua, viceversa gli assegno una stringa vuota
                 score: media.vote_average
               }
             } else if (searchingFor == "tv") {
               var context = {                           //ad ogni iterazione sovrascrivo nell'oggetto context le proprieta di media di cui ho bisogno
                 title: media.name,
                 originalTitle: media.original_name,
-                language: media.original_language,
+                flag: flagIt(media.original_language),
+                language: (flagIt(media.original_language) === "") ? media.original_language : "",
                 score: media.vote_average
               }
             }
